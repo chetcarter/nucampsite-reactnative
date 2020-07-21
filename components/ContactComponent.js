22addf89cae8f81ac35e68670a6791643c96ba82
Chet Carter <abazabalv@gmail.com>
2020-07-21 11:12:51 -0600

    Social Sharing

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -5,44 +5,46 @@ import * as Animatable from 'react-native-animatable';
 import * as MailComposer from 'expo-mail-composer';
 
 class Contact extends Component {
-  static navigationOptions = {
-    title: 'Contact Us',
-  };
+	static navigationOptions = {
+		title: 'Contact Us',
+	};
 
-  sendMail() {
-    MailComposer.composeAsync({
-      recipients: ['campsite@nucamp.co'],
-      subject: 'Inquiry',
-      body: 'To whom it may concern:'
-    });
-  }
+	sendMail() {
+		MailComposer.composeAsync({
+			recipients: ['campsite@nucamp.co'],
+			subject: 'Inquiry',
+			body: 'To whom it may concern:',
+		});
+	}
 
-  render() {
-    return (
-      <ScrollView>
-        <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
-          <Card title='Contact Information' wrapperStyle={{ margin: 20 }}>
-            <Text>1 NuCamp Way</Text>
-            <Text>Seattle, WA 98001</Text>
-            <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
-            <Text>Phone: 1-206-555-1234</Text>
-            <Text>Email: campsite@nucamp.co</Text>
-            <Button 
-              title='Send Email'
-              buttonStyle={{backgroundColor: '#5637DD', margin: 40}}
-              icon={<Icon
-                name='envelope-o'
-                type='font-awesome'
-                color='#FFF'
-                iconStyle={{marginRight: 10}}
-              />}
-              onPress={() => this.sendMail()}
-            />
-          </Card>
-        </Animatable.View>
-      </ScrollView>
-    );
-  }
+	render() {
+		return (
+			<ScrollView>
+				<Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+					<Card title='Contact Information' wrapperStyle={{ margin: 20 }}>
+						<Text>1 NuCamp Way</Text>
+						<Text>Seattle, WA 98001</Text>
+						<Text style={{ marginBottom: 10 }}>U.S.A.</Text>
+						<Text>Phone: 1-206-555-1234</Text>
+						<Text>Email: campsite@nucamp.co</Text>
+						<Button
+							title='Send Email'
+							buttonStyle={{ backgroundColor: '#5637DD', margin: 40 }}
+							icon={
+								<Icon
+									name='envelope-o'
+									type='font-awesome'
+									color='#FFF'
+									iconStyle={{ marginRight: 10 }}
+								/>
+							}
+							onPress={() => this.sendMail()}
+						/>
+					</Card>
+				</Animatable.View>
+			</ScrollView>
+		);
+	}
 }
 
 export default Contact;
